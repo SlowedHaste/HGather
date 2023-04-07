ab80416c02d8321ebdf48fdc6959c6cc396708d3
samson-ffxi <130154563+samson-ffxi@users.noreply.github.com>
2023-04-07 09:28:24 -0500

    Update hgather.lua
    spacing cleanup

--- hgather.lua
+++ hgather.lua
@@ -7,14 +7,12 @@ addon.commands = {'/hgather'};
 ----------------------------------------------------------------------------------------------------
 -- Variables
 ----------------------------------------------------------------------------------------------------
-
 local common = require('common');
 local imgui = require('imgui');
 local settings = require('settings');
 local ffi = require('ffi');
 local d3d = require 'd3d8';
 local d3d8dev   = d3d.get_device();
-
 
 local ashitaResourceManager = AshitaCore:GetResourceManager();
 local ashitaChatManager     = AshitaCore:GetChatManager();
@@ -35,11 +33,9 @@ hgather = T{
     pricing = { }
 };
 
-
 ----------------------------------------------------------------------------------------------------
 -- Helper Functions
 ----------------------------------------------------------------------------------------------------
-
 function file_exists(file)
     local f = io.open(file, "rb")
     if f then f:close() end
@@ -90,7 +86,6 @@ end
 ----------------------------------------------------------------------------------------------------
 -- Load Event
 ----------------------------------------------------------------------------------------------------
-
 ashita.events.register('load', 'load_cb', function()
     updatePricing()
 end)
@@ -98,9 +93,7 @@ end)
 ----------------------------------------------------------------------------------------------------
 -- Commands
 ----------------------------------------------------------------------------------------------------
-
 ashita.events.register('text_out', 'text_out_callback1', function (e)
-
     if (not e.injected) then
         if (string.match(e.message, '/hgather reset')) then
             hgather.diggingRewards = { }
@@ -109,7 +102,6 @@ ashita.events.register('text_out', 'text_out_callback1', function (e)
             print('HGather: Digging session has been reset')
         end
     end
-
 
     if (not e.injected) then
         if (string.match(e.message, '/hgather open')) then
@@ -135,23 +127,18 @@ ashita.events.register('text_out', 'text_out_callback1', function (e)
             print('HGather: Commands are Open, Close, Reset, Update, Help')
         end
     end
-
-
 end);
 
 ----------------------------------------------------------------------------------------------------
 -- Parse Digging Items + Main Logic
 ----------------------------------------------------------------------------------------------------
-
 ashita.events.register('text_in', 'text_in_cb', function (e)
     message = e.message;
     message = string.lower(message);
     message = string.strip_colors(message);
 
-  
     success = string.match(message, "obtained: (.*).") or successBreak
     unable = string.contains(message, "you dig and you dig");
-
 
     if (success or unable) then
         hgather.isAttempt = true
@@ -160,69 +147,55 @@ ashita.events.register('text_in', 'text_in_cb', function (e)
     end
    
 
-        if hgather.isAttempt then 
-            successBreak = false;
-            success = string.match(message, "obtained: (.*).") or successBreak
-            unable = string.contains(message, "you dig and you dig");
-            broken = false;
-            lost = false;
+    if hgather.isAttempt then 
+        successBreak = false;
+        success = string.match(message, "obtained: (.*).") or successBreak
+        unable = string.contains(message, "you dig and you dig");
+        broken = false;
+        lost = false;
 
-            --keep window open
-            if (unable or success) then
-                hgather.open = true;
-            end
-
-            --count attempt
-            if (unable) then 
-                hgather.numDigs = hgather.numDigs + 1;
-            end
-        
-            if success then
-                --local of = string.match(success, "of (.*)");
-                --if of then success = of; txt = of end;
-                hgather.numItems = hgather.numItems + 1;
-                hgather.numDigs = hgather.numDigs + 1;
-
-                if (success ~= nil) then
-                    if (hgather.diggingRewards[success] == nil) then
-                        hgather.diggingRewards[success] = 1
-                    elseif (hgather.diggingRewards[success] ~= nil) then
-                        hgather.diggingRewards[success] = hgather.diggingRewards[success] + 1
-                    end
-                end
-
-
-
-            end
-
-
+        --keep window open
+        if (unable or success) then
+            hgather.open = true;
         end
-    
 
- 
+        --count attempt
+        if (unable) then 
+            hgather.numDigs = hgather.numDigs + 1;
+        end
+        
+        if success then
+            --local of = string.match(success, "of (.*)");
+            --if of then success = of; txt = of end;
+            hgather.numItems = hgather.numItems + 1;
+            hgather.numDigs = hgather.numDigs + 1;
 
-
+            if (success ~= nil) then
+                if (hgather.diggingRewards[success] == nil) then
+                    hgather.diggingRewards[success] = 1
+                elseif (hgather.diggingRewards[success] ~= nil) then
+                    hgather.diggingRewards[success] = hgather.diggingRewards[success] + 1
+                end
+            end
+        end
+    end
 end)
 
 ----------------------------------------------------------------------------------------------------
 -- Digging Event
 ----------------------------------------------------------------------------------------------------
-
 ashita.events.register('packet_out', 'packet_out_callback1', function (e)
     if e.id == 0x01A then -- digging
-            if struct.unpack("H", e.data_modified, 0x0A) == 0x1104 then -- digging
-                hgather.isAttempt = true;
-                hgather.lastDig = os.time();
-            end
-       
+        if struct.unpack("H", e.data_modified, 0x0A) == 0x1104 then -- digging
+            hgather.isAttempt = true;
+            hgather.lastDig = os.time();
+        end
     end
 end)
-
 
 ----------------------------------------------------------------------------------------------------
 -- GUI
 ----------------------------------------------------------------------------------------------------
-
 ashita.events.register('d3d_present', 'present_cb', function () 
     lasttime = os.difftime(os.time(), hgather.lastDig);
     if (hgather.open == false) then
@@ -233,28 +206,25 @@ ashita.events.register('d3d_present', 'present_cb', function ()
     imgui.SetNextWindowSize({ 250, -1, }, ImGuiCond_Always);
 
     if (imgui.Begin('HasteGather', hgather.open, bit.bor(ImGuiWindowFlags_NoDecoration, ImGuiWindowFlags_AlwaysAutoResize, ImGuiWindowFlags_NoSavedSettings, ImGuiWindowFlags_NoFocusOnAppearing, ImGuiWindowFlags_NoNav))) then
-            totalworth = 0
+        totalworth = 0
         
-            imgui.Text('~~ Digging Session ~~')
-            imgui.Text("Attempted Digs: " + hgather.numDigs)
-            imgui.Text('Items Dug: ' + hgather.numItems)
-            imgui.Separator();
+        imgui.Text('~~ Digging Session ~~')
+        imgui.Text("Attempted Digs: " + hgather.numDigs)
+        imgui.Text('Items Dug: ' + hgather.numItems)
+        imgui.Separator();
 
-            for k,v in pairs(hgather.diggingRewards) do
-
-                if (hgather.pricing[k] ~= nil) then
-                    totalworth = totalworth + hgather.pricing[k]
-                end
-
-                
-                imgui.Text(k + ": " + "x" + v)
+        for k,v in pairs(hgather.diggingRewards) do
+            if (hgather.pricing[k] ~= nil) then
+                totalworth = totalworth + hgather.pricing[k]
             end
 
-            imgui.Separator();
-            imgui.Text("Gil Made: " + totalworth + 'g')
+            imgui.Text(k + ": " + "x" + v)
+        end
 
-            --List things gotten for digging session
+        imgui.Separator();
+        imgui.Text("Gil Made: " + totalworth + 'g')
 
+        --List things gotten for digging session
     end
 
     --end session
@@ -262,9 +232,4 @@ ashita.events.register('d3d_present', 'present_cb', function ()
         imgui.End()
         hgather.open = false;
     end
-
-   
 end)
-
-
-
